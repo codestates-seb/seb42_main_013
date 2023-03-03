97ea788a91570053a914f80430f091664feac7d4
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2023-03-04 04:39:16 +0900

    [Feat] 디렉토리 추가

---
+++ team13/server/src/main/java/com/SebMainTeam13/team13/Team13Application.java
@@ -0,0 +1,13 @@
+package com.SebMainTeam13.team13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team13Application.class, args);
+	}
+
+}
