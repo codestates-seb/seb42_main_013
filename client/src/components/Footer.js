11804a8fbf3054b28a44ba49467865fee391bcd0
yjyaang <topbuty97@gmail.com>
2023-03-09 17:54:45 +0900

    [Design] Footer 초기 css 작업

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarCheck } from "@fortawesome/free-regular-svg-icons";
+import { faPills } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const FooterContainer = styled.div`
   background-color: #ffffff;
@@ -7,13 +12,47 @@ const FooterContainer = styled.div`
   height: 64px;
   position: fixed;
   bottom: 0;
-  border: 1px solid #000000;
+  border-top: 1px solid #999999;
+  padding: 8px;
+  display: flex;
+  justify-content: space-around;
+`
+
+const FooterCatDiv = styled.div`
+  padding: 4px;
+  color: #464646;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  cursor: pointer;
+  .footer-icon {
+    width: 25px;
+    height: 25px;
+    margin: 4px 0;
+  }
 `
 
 function Footer() {
   return (
     <FooterContainer>
-      나 Footer임
+      <FooterCatDiv>
+        <FontAwesomeIcon icon={faCalendarCheck} className="footer-icon" />
+        <div className="footer-text">달력관리</div>
+      </FooterCatDiv>
+      <FooterCatDiv>
+        <FontAwesomeIcon icon={faMagnifyingGlass} className="footer-icon" />
+        <div className="footer-text">추천/검색</div>
+      </FooterCatDiv>
+      <FooterCatDiv>
+        <FontAwesomeIcon icon={faPills} className="footer-icon" />
+        <div className="footer-text">알약관리</div>
+      </FooterCatDiv>
+      <FooterCatDiv>
+        <FontAwesomeIcon icon={faUser} className="footer-icon" />
+        <div className="footer-text">마이페이지</div>
+      </FooterCatDiv>
     </FooterContainer>
   )
 }
