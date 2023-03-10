d582f971757a2baa1c6283c7fbb6546d3d05c1a4
yjyaang <topbuty97@gmail.com>
2023-03-10 23:07:56 +0900

    [Feat] 슬라이드 기능 구현(라이브러리 활용)

--- client/src/components/ScrollBar.js
+++ client/src/components/ScrollBar.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
-import { useState } from "react";
-import useWindowSize from "../util/useWindowSize";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleChevronLeft } from "@fortawesome/free-solid-svg-icons";
-import { faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { health } from "./Health";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+
+SwiperCore.use([Navigation]);
 
 const ScrollBarContainer = styled.div`
   height: 100px;
@@ -15,44 +16,20 @@ const ScrollBarContainer = styled.div`
   margin-left: 0;
   white-space: nowrap;
   overflow-x: scroll;
-  .scroll-button {
-    color: var(--black-300);
-    width: 20px;
-    height: 20px;
+  .category {
+    font-size: 13px;
     cursor: pointer;
-  }
-  .left {
-    position: absolute;
-    left: 10px;
-    z-index: 10;
-  }
-  .right {
-    position: absolute;
-    right: 10px;
-    z-index: 10;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
   .selected-area {
     font-weight: 600;
   }
   .category-select {
     background-color: rgba(91, 133, 235, 0.8);
-    /* border: 2px solid var(--blue-100); */
   }
-  ::-webkit-scrollbar {
-    display: none;
-  }
-`
-
-const CategoryDiv = styled.div`
-  margin: 0 8px;
-  font-size: 13px;
-  cursor: pointer;
-  transform: ${(props) => props.slidePx ? `translateX(${props.slidePx}px)` : "none"};
-  transition: 0.5s ease;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
 `
 
 const CategoryIconDiv = styled.div`
@@ -71,37 +48,24 @@ const CategoryIconDiv = styled.div`
 `
 
 function ScrollBar() {
-  const [slidePx, setSlidePx] = useState(0);
-  const size = useWindowSize();
-  let moveWidth;
-  if(size.width > 428) {
-    moveWidth = 428;
-  } else {
-    moveWidth = size.width;
-  }
-
-  const toPrev = () => {
-    if (slidePx < 0) setSlidePx(slidePx + moveWidth - 120);
-  };
-
-  const toNext = () => {
-    if (slidePx > -1200) setSlidePx(slidePx - moveWidth + 120);
-  };
 
   return (
     <ScrollBarContainer>
-      <FontAwesomeIcon icon={faCircleChevronLeft} className="scroll-button left" onClick={toPrev} />
-      {health.map(el => {
-        return (
-          <CategoryDiv key={el.id} slidePx={slidePx} className={el.title === "눈건강" ? "selected-area" : ""}>
-            <CategoryIconDiv className={el.title === "눈건강" ? "category-select" : ""}>
-              <img src={el.src} alt="health-icon"/>
-            </CategoryIconDiv>
-            {el.title}
-          </CategoryDiv>
-        )
-      })}
-      <FontAwesomeIcon icon={faCircleChevronRight} className="scroll-button right" windowwidth={size.width} onClick={toNext}/>
+      <Swiper
+        spaceBetween={4}
+        slidesPerView={5}
+      >
+        {health.map(el => {
+          return (
+            <SwiperSlide key={el.id} className={el.title === "눈건강" ? "selected-area category" : "category"}>
+              <CategoryIconDiv className={el.title === "눈건강" ? "category-select" : ""}>
+                <img src={el.src} alt="health-icon" />
+              </CategoryIconDiv>
+              {el.title}
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
     </ScrollBarContainer>
   )
 }
