5261375d365f95c830f8be21cea1624bb6ce0e66
yjyaang <topbuty97@gmail.com>
2023-03-14 09:50:26 +0900

    [Design] Footer 페이지 이동 기능 구현 및 카테고리 데이터, 스크롤바 수정

--- client/src/components/ScrollBar.js
+++ client/src/components/ScrollBar.js
@@ -2,11 +2,8 @@ import styled from "styled-components";
 import { useState } from "react";
 import { health } from "./Health";
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
-
-SwiperCore.use([Navigation]);
 
 const ScrollBarContainer = styled.div`
   height: 100px;
@@ -52,7 +49,7 @@ function ScrollBar() {
   const [isClicked, setIsClicked] = useState("영양보충");
 
   const clickHandler = (e) => {
-    console.log(e.target);
+    setIsClicked(e.currentTarget.id);
   }
 
   return (
@@ -63,11 +60,11 @@ function ScrollBar() {
       >
         {health.map(el => {
           return (
-            <SwiperSlide key={el.id} className={el.title === "눈건강" ? "selected-area category" : "category"} onClick={clickHandler}>
-              <CategoryIconDiv className={el.title === "눈건강" ? "category-select" : ""}>
+            <SwiperSlide key={el.id} className={el.title === isClicked ? "selected-area category" : "category"} onClick={clickHandler} id={el.title}>
+              <CategoryIconDiv className={el.title === isClicked ? "category-select" : ""}>
                 <img src={el.src} alt="health-icon" />
               </CategoryIconDiv>
-              {el.title}
+              <div>{el.title}</div>
             </SwiperSlide>
           )
         })}
