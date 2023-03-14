f1c9b699fc580790278520e82e87998bddc1d249
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2023-03-14 15:26:40 +0900

    [Fix] 충돌한 브랜치 병합 위해 기존 파일 삭제

--- server/src/main/java/com/SebMainTeam13/team13/security/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.SebMainTeam13.team13.security.dto;
-
-import lombok.Getter;
-
-@Getter
-public class LoginDto {
-    private String email;
-    private String password;
-}
--- server/src/main/java/com/SebMainTeam13/team13/security/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.SebMainTeam13.team13.security.repository;
-
-import com.SebMainTeam13.team13.security.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByRoleName(String roleName);
-}
