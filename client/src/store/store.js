cb9167c0204921c562fd1ad0d1c693560cd39484
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2023-03-16 20:26:49 +0900

    [Fix] 충돌 수정

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import dataCreateReducer from "../reducer/dataCreateReducer";
 import searchReducer from "../reducer/searchReducer";
+import concernReducer from "../reducer/concernReducer";
 
 
 
 const store = configureStore({
   reducer: {
     searchReducer,
+    concernReducer,
     create: dataCreateReducer,
   }
 })
