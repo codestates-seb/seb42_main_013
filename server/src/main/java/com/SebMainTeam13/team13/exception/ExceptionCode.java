ee9b408813199b8571f11c325641982b25182dbb
SEB-BE-42-mkcho <115971051+SEB-BE-42-mkcho@users.noreply.github.com>
2023-03-17 10:30:29 +0900

    Revert "Feat/be/concern"

--- server/src/main/java/com/SebMainTeam13/team13/exception/ExceptionCode.java
+++ server/src/main/java/com/SebMainTeam13/team13/exception/ExceptionCode.java
@@ -8,12 +8,9 @@ public enum ExceptionCode {
     NOT_IMPLEMENTATION(501, "Not Implementation"),
     INVALID_MEMBER_STATUS(400, "Invalid member status"),
     DETAIL_NOT_FOUND(404, "Question not found"),
-    DETAIL_EXISTS(409, "Detail already exists"),
-    CONCERN_NOT_FOUND(404, "Concern not found"),
     SUPPLEMENT_NOT_FOUND(404, "Supplement not found"),
     DETAIL_SUPPLEMENT_NOT_FOUND(404, "DetailSupplement not found"),
     DETAIL_EXISTS(409, "Detail already exists");
-
     @Getter
     private int status;
 
