7cf8f98caa6bd96f73d92990b3f4bc497e96de33
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2023-03-20 15:13:35 +0900

    [Feat] 배포를 위한 사전작업

---
+++ server/src/main/java/com/SebMainTeam13/team13/db/DBController.java
@@ -0,0 +1,18 @@
+package com.SebMainTeam13.team13.db;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+
+//@Component
+//@RequiredArgsConstructor
+//public class DBController implements CommandLineRunner {
+//    private final JdbcTemplate jdbcTemplate;
+//        @Override
+//       public void run(String... args) {
+//        jdbcTemplate.execute("DROP DATABASE dbformain13");
+//        jdbcTemplate.execute("CREATE DATABASE dbformain13");
+//    }
+//}
