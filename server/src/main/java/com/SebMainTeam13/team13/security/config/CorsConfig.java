c03e8e6b8327520a88c91f317bbef398152d187c
SEB-BE-42-mkcho <withmove@gmail.com>
2023-03-21 16:56:30 +0900

    [Feat] CorsConfig setAllowedHeaders 추가

--- server/src/main/java/com/SebMainTeam13/team13/security/config/CorsConfig.java
+++ server/src/main/java/com/SebMainTeam13/team13/security/config/CorsConfig.java
@@ -6,12 +6,14 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "PATCH", "DELETE","OPTIONS"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
