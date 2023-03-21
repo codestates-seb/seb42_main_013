152f4bcb3c9001874cfd7b240294455d4d394cbd
taeeunkim <01053702613a@gmail.com>
2023-03-22 00:52:01 +0900

    [Feat] TimeLine.js Login.js Signup.js 컴포넌트 수정

--- client/src/components/Timeline.js
+++ client/src/components/Timeline.js
@@ -1,10 +1,11 @@
 import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
-import { useDrag } from 'react-dnd';
+// import { useDrag } from 'react-dnd';
+import { current } from "@reduxjs/toolkit";
 
 const TimelineContainer = styled.div`
   width: 100%; max-width: 428px;
-  height: 100%;
+  /* height: 100%; */
   /* background-color: #ffffff; */
   /* background-color: gray; */
   padding: var(--gap-lg); //24px
@@ -29,31 +30,45 @@ const TimeContentContainer = styled.div`
     display: none;
   }
 `;
+const TimeWrap = ({ time, hours, minutes, supplements, selectedDayFormat }) => {
+  const isSameHours = time.split(":")[0] === String(hours);
 
-const TimeWrap = ({ time, data }) => {
 
+// console.log(supplements[0].startDate)
+  // console.log(typeof supplements[0].startDate) //string
+  // console.log(typeof selectedDayFormat)
   return (
-    <>
-      <Container>
-        <TimeWrapCss>
-          <div style={{ display: "flex" }}>
-            <TimeDiv>{time}</TimeDiv>
-            <TimeSpot />
+    <Container>
+      <TimeDivider>
+        {Array.from({ length: 60 }, (v, i) => {
+          return <div key={i} style={{ backgroundColor: isSameHours && i === minutes ? "#EB4233" : "unset" }}>
+            {(isSameHours && i === minutes) && <div style={{ position: "absolute", width: "10px", height: "10px", backgroundColor: "#EB4233", left: "-5.5px", top: "-4.5px", borderRadius: "50%", zIndex: 1 }} />}
           </div>
-          <Line></Line>
-        </TimeWrapCss>
-        <InfoDiv>
-          <CardWrap>
-            <div>오메가 3</div>
-            <div>take 1 pill</div>
-          </CardWrap>
-          <CardWrap>
-            <div>비타민</div>
-            <div>take 1 pill</div>
-          </CardWrap>
-        </InfoDiv>
-      </Container>
-    </>
+        })}
+      </TimeDivider>
+      <TimeWrapCss>
+        <div style={{ display: "flex" }}>
+          <TimeDiv>{time}</TimeDiv>
+          <TimeSpot />
+        </div>
+        <HorizontalLine></HorizontalLine>
+      </TimeWrapCss>
+      <InfoDiv>
+        {
+        supplements.map((e) => {
+          if((e.startDate === selectedDayFormat) && (e.takingTime.map((e) => e.split(":")[0]).indexOf(time.split(":")[0]) !== -1)) {
+            return <CardWrap>
+              <div>{e.supplementName}</div>
+              <div>take {e.take} pill</div>
+            </CardWrap>
+          }else{
+            <CardWrap>
+              <div style={{height:"100px", width: "100%"}}>{`d`}</div>
+            </CardWrap>
+          }
+        })}
+      </InfoDiv>
+    </Container>
   )
 }
 const Container = styled.div`
@@ -66,6 +81,20 @@ const Container = styled.div`
   flex: 1;
 `;
 
+const TimeDivider = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-between;
+  div{
+    width: 76%;
+    height: 1px;
+    position: relative;
+  }
+`;
 const TimeWrapCss = styled.div`
   display: flex;
   justify-content: flex-end;
@@ -86,7 +115,7 @@ const TimeSpot = styled.div`
   border-radius: 50%;
   z-index: 1;
 `;
-const Line = styled.div`
+const HorizontalLine = styled.div`
   position: absolute;
   left: 25%;
   width: 1px;
@@ -118,24 +147,51 @@ const CardWrap = styled.div`
   }
 `;
 
+const VerticalLine = styled.div`
+  position: absolute;
+  top: 75%;
+  right: 13%;
+  width: 55%;
+  height: 1px;
+  background-color: #EB4233;
+`;
 
 
-function Timeline({ nowYear, nowMonth, nowDay, nowDate, setNowYear, setNowMonth, setNowDay }) {
+function Timeline({ supplements, nowYear, nowMonth, nowDay, nowDate, setNowYear, setNowMonth, setNowDay }) {
   const weeks = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  const time = Array.from({ length: 26 }, (v, i) => String(i).length > 1 ? `${i}:00` : `0${i}:00`);
+  const timeSpots = Array.from({ length: 25 }, (v, i) => String(i).length > 1 ? `${i}:00` : `0${i}:00`);
   const scrollRef = useRef();
-  let hours = new Date().getHours();
-  const minutes = new Date().getMinutes();
+  let time = new Date();
+  const [count, setCount] = useState(`${time.getHours()}시 ${time.getMinutes()}분 ${time.getSeconds()}초`);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const selectedDayFormat=`${nowYear}-${nowMonth < 10 ? `0${nowMonth}` : nowMonth}-${nowDate < 10 ? `0${nowDate}` : nowDate}`;
+  // console.log(supplements)
+  // scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
   useEffect(() => {
-    // scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-    scrollRef.current.scrollTop = hours * 100;
-  }, [])
+    const getTime = () => {
+      let time = new Date();
+      // console.log(time)
+      // scrollRef.current.scrollTop = time.getHours() * 135;
+      setHours(time.getHours())
+      setMinutes(time.getMinutes())
+      setCount(`${time.getHours()}시 ${time.getMinutes()}분 ${time.getSeconds()}초`);
+    }
+    getTime();
+    // let id = setInterval(() => getTime(), 1000);
+    let id = setInterval(getTime, 1000);
+    return () => clearInterval(id);
+  }, []);
+
   return (
     <TimelineContainer>
-      <TitleContent>{`${nowMonth < 10 ? `0${nowMonth}` : nowMonth}.${nowDate < 10 ? `0${nowDate}` : nowDate}.${weeks[nowDay]}.`}</TitleContent>
+      <TitleContent>{`${nowMonth < 10 ? `0${nowMonth}` : nowMonth}.${nowDate < 10 ? `0${nowDate}` : nowDate}.${weeks[nowDay]}.`}
+      </TitleContent>
       <TimeContentContainer ref={scrollRef}>
-        {time.map((e) => {
-          return <TimeWrap key={e} time={e}></TimeWrap>
+        {/* <VerticalLine /> */}
+        {timeSpots.map((e, idx) => {
+          // hours, minutes가 변경될 떄마다 rerendering
+          return <TimeWrap key={e} time={e} hours={hours} minutes={minutes} supplements={supplements} selectedDayFormat={selectedDayFormat}></TimeWrap>
         })}
       </TimeContentContainer>
     </TimelineContainer>
