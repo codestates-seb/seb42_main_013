b4d60eec57bfc8f593becc7d6150c56b58268034
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2023-03-25 09:53:23 +0900

    [Feat]detailSupplement delete시 Transactional 삭제

--- server/src/main/java/com/SebMainTeam13/team13/detailSupplement/service/DetailSupplementService.java
+++ server/src/main/java/com/SebMainTeam13/team13/detailSupplement/service/DetailSupplementService.java
@@ -1,12 +1,14 @@
 package com.SebMainTeam13.team13.detailSupplement.service;
 
 import com.SebMainTeam13.team13.detail.entity.Detail;
+import com.SebMainTeam13.team13.detail.repository.DetailRepository;
 import com.SebMainTeam13.team13.detail.service.DetailService;
 import com.SebMainTeam13.team13.detailSupplement.entity.DetailSupplement;
 import com.SebMainTeam13.team13.detailSupplement.repository.DetailSupplementRepository;
 import com.SebMainTeam13.team13.exception.BusinessLogicException;
 
 import com.SebMainTeam13.team13.supplement.entity.Supplement;
+import com.SebMainTeam13.team13.supplement.repository.SupplementRepository;
 import com.SebMainTeam13.team13.supplement.service.SupplementService;
 import com.SebMainTeam13.team13.user.entity.User;
 import com.SebMainTeam13.team13.user.repository.UserRepository;
@@ -28,12 +30,15 @@ import static com.SebMainTeam13.team13.exception.ExceptionCode.DETAIL_SUPPLEMENT
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class DetailSupplementService {
     private final DetailSupplementRepository detailDetailSupplementRepository;
     private final DetailService detailService;
     private final UserRepository userRepository;
     private final UserService userservice;
     private final SupplementService supplementService;
+    private final DetailRepository detailRepository;
+    private final SupplementRepository supplementRepository;
 
     public DetailSupplement createDetailSupplement(DetailSupplement detailSupplement,Long userId){
         User user = userservice.getUser(userId);
@@ -81,15 +86,21 @@ public class DetailSupplementService {
 
         return verifiedDetailSupplement;
     }
-
-    public void deleteDetailSupplement(DetailSupplement detailSupplement,Long userId){
-       User user = userRepository.findById(userId).get();
-       if(user.getDetail().getDetailSupplements().contains(detailSupplement) ) {
-           detailDetailSupplementRepository.delete(detailSupplement);
-       }
-       else throw new RuntimeException("DetailSupplement not found for user");
+    @Transactional
+    public void deleteDetailSupplement(DetailSupplement detailSupplement, Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+        Detail detail = detailSupplement.getDetail();
+        Supplement supplement = detailSupplement.getSupplement();
+        if (user.getDetail().getDetailSupplements().contains(detailSupplement)) {
+            detail.getDetailSupplements().remove(detailSupplement);
+            supplement.getDetailSupplements().remove(detailSupplement);
+            detailDetailSupplementRepository.delete(detailSupplement);
+            detailRepository.save(detail);
+            supplementRepository.save(supplement);
+        } else {
+            throw new RuntimeException("DetailSupplement not found for user");
+        }
     }
-
     //#### 내부 메서드 ###//
 
     private void  verifyDetailSupplementByDetailAndSupplement(DetailSupplement detailSupplement) {
