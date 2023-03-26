eebc7fde35f3643fe68feac4f64fc4a58813bd49
yjyaang <topbuty97@gmail.com>
2023-03-26 21:00:29 +0900

    [Fix] 마이페이지 토큰 유효기간 관련 에러 핸들링, 추천 페이지 DB 자료로 변경

--- client/src/components/Health.js
+++ client/src/components/Health.js
@@ -3,336 +3,100 @@ export const health = [
     id: 1,
     title: "영양보충",
     src: `images/healthicons/nutrition.png`,
-    supplementsList: [
-      {supplementName: "종합비타민", imageURL: "https://shopping-phinf.pstatic.net/main_1735391/17353911012.20201120160601.jpg"},
-      {supplementName: "비오틴", imageURL: "https://shopping-phinf.pstatic.net/main_3059076/30590766618.20221117103035.jpg"},
-      {supplementName: "마그네슘", imageURL: "https://shopping-phinf.pstatic.net/main_1309871/13098713037.20210805172525.jpg"},
-      {supplementName: "엽산", imageURL: "https://shopping-phinf.pstatic.net/main_3828576/38285763618.20230227090321.jpg"}
-    ],
-    contents: [
-      "배달음식 그만! 맛있는 집밥으로 영양도 챙겨요",
-      "맛있는 고기로 단백질도 잊지 말고 섭취해 주세요",
-      "든든하게 영양보충하고\n집 앞 산책 30분 함께해요!",
-      "건강에 좋은 야채도 든든히 먹기!"
-    ]
   },
   {
     id: 2,
     title: "눈건강",
     src: `images/healthicons/eye.png`,
-    supplementsList: [
-      {supplementName: "루테인 지아잔틴", imageURL: "https://shopping-phinf.pstatic.net/main_2210312/22103121641.20211122100054.jpg"},
-      {supplementName: "오메가3", imageURL: "https://shopping-phinf.pstatic.net/main_2491614/24916146522.20201120102440.jpg"},
-      {supplementName: "비타민A", imageURL: "https://shopping-phinf.pstatic.net/main_1735391/17353911012.20201120160601.jpg"},
-      {supplementName: "빌베리", imageURL: "https://shopping-phinf.pstatic.net/main_2710561/27105610528.20210511123510.jpg"}
-    ],
-    contents: [
-      "맛도 좋고\n눈에도 좋은 블루베리를 섭취하세요!",
-      "지금 이 글을\n보고 계시다면, 하늘을 바라보며 10분 휴식!",
-      "눈의 건조함 막아주는\n인공 눈물 사용을 추천드려요",
-      "오늘 저녁 반찬은 눈에 좋은 당근볶음을 만들어 볼까요?"
-    ]
   },
   {
     id: 3,
     title: "면역력",
     src: `images/healthicons/shield.png`,
-    supplementsList: [{supplementName: "종합비타민", imageURL: "https://shopping-phinf.pstatic.net/main_1735391/17353911012.20201120160601.jpg"},
-    {supplementName: "프로폴리스", imageURL: "https://shopping-phinf.pstatic.net/main_3129732/31297322631.20220314114144.jpg"},
-    {supplementName: "아연", imageURL: "https://shopping-phinf.pstatic.net/main_1860252/18602526124.20200814113559.jpg"},
-    {supplementName: "프로바이오틱스", imageURL: "https://shopping-phinf.pstatic.net/main_3505461/35054619618.20221005080625.jpg"}
-  ],
-    contents: [
-      "면역력이 걱정인 당신,\n매일 아침 홍삼스틱 하나 어떠신가요?",
-      "항암, 항염 작용에 좋은 마늘,\n면역력을 위해\n챙겨 드세요",
-      "스트레스는\n만병의 근원, 나만의 스트레스 해소법 찾기!",
-      "체온을 올리는\n운동도 꾸준히 진행해 주세요!"
-    ]
   },
   {
     id: 4,
     title: "간건강",
     src: `images/healthicons/liver.png`,
-    supplementsList: [
-      {supplementName: "밀크씨슬", imageURL: "https://shopping-phinf.pstatic.net/main_1977116/19771162299.20230313120229.jpg"},
-    {supplementName: "실리마린", imageURL: "https://shopping-phinf.pstatic.net/main_2051460/20514608589.20190805124639.jpg"},
-    {supplementName: "아르기닌", imageURL: "https://shopping-phinf.pstatic.net/main_2812726/28127266522.20221012114128.jpg"},
-    {supplementName: "UDCA", imageURL: "https://shopping-phinf.pstatic.net/main_8417527/84175274034.jpg"}
-  ],
-    contents: [
-      "건강한 간을 위해 술은 조금만 드세요!",
-      "과유불급!\n약을 과다복용\n하지 않도록\n주의해 주세요",
-      "간 건강에도\n꾸준한 운동은\n많은 도움이 됩니다!",
-      "양질의 단백질과\n균형 잡힌 식사도 잊지 마세요"
-    ]
   },
   {
     id: 5,
     title: "피로회복",
     src: `images/healthicons/power.png`,
-    supplementsList: [
-      {supplementName: "종합비타민", imageURL: "https://shopping-phinf.pstatic.net/main_1735391/17353911012.20201120160601.jpg"},
-      {supplementName: "홍삼", imageURL: "https://shopping-phinf.pstatic.net/main_1125965/11259655619.20210323174551.jpg"},
-      {supplementName: "마그네슘", imageURL: "https://shopping-phinf.pstatic.net/main_1309871/13098713037.20210805172525.jpg"},
-      {supplementName: "비타민B", imageURL: "https://shopping-phinf.pstatic.net/main_1735391/17353911012.20201120160601.jpg"}
-    ],
-    contents: [
-      "충분한 수면으로\n몸이 회복할 수 있도록 해주세요",
-      "적당히 움직이는 것이 오히려 피로회복에\n더 좋다는 사실!",
-      "스트레스는\n만병의 근원, 나만의 스트레스 해소법 찾기!",
-      "나만의 루틴으로 규칙적인 생활을 지속해 주세요!"
-    ]
   },
   {
     id: 6,
     title: "장건강",
     src: `images/healthicons/intestine.png`,
-    supplementsList: [
-      {supplementName: "프로바이오틱스", imageURL: "https://shopping-phinf.pstatic.net/main_3505461/35054619618.20221005080625.jpg"},
-      {supplementName: "프리바이오틱스", imageURL: "https://shopping-phinf.pstatic.net/main_2864100/28641004554.20230309095405.jpg"},
-      {supplementName: "차전자피", imageURL: "https://shopping-phinf.pstatic.net/main_2675726/26757266524.20221129172915.jpg"},
-      {supplementName: "마그네슘", imageURL: "https://shopping-phinf.pstatic.net/main_1309871/13098713037.20210805172525.jpg"}
-    ],
-    contents: [
-      "건강한 장을 위한 수분 섭취!\n지금 바로 물 한 잔을 마셔 주세요!",
-      "아침 식사로\n장에 좋은 사과 1개 먹는 건 어떠신가요?",
-      "너무 매운 음식과 너무 많은 커피는\n장 건강에 좋지 않아요",
-      "끼니 거르지 말고 일정한 시간에\n챙겨 드세요!"
-    ]
   },
   {
     id: 7,
     title: "체지방감소",
     src: `images/healthicons/weight.png`,
-    supplementsList: [
-      {supplementName: "녹차카테킨", imageURL: "https://shopping-phinf.pstatic.net/main_2460735/24607359522.20230316114352.jpg"},
-      {supplementName: "CLA", imageURL: "https://shopping-phinf.pstatic.net/main_2108214/21082141177.20200110112341.jpg"},
-      {supplementName: "가르시니아", imageURL: "https://shopping-phinf.pstatic.net/main_1329417/13294174020.20230316090601.jpg"},
-      {supplementName: "L-카르니틴", imageURL: "https://shopping-phinf.pstatic.net/main_2090924/20909247470.20220816165032.jpg"}
-    ],
-    contents: [
-      "과도한 탄수화물을 줄이고\n단백질을 섭취해 주세요!",
-      "꾸준한 유산소 운동은 체지방 감소의 지름길",
-      "달달한 간식은\n잠시 내려두고\n건강한 간식으로 바꾸기!",
-      "나만의 식단 일기장에 식단을 꾸준히 기록하세요"
-    ]
   },
   {
     id: 8,
     title: "혈행개선",
     src: `images/healthicons/blood.png`,
-    supplementsList: [
-      {supplementName: "오메가3", imageURL: "https://shopping-phinf.pstatic.net/main_2491614/24916146522.20201120102440.jpg"},
-      {supplementName: "비타민E", imageURL: "https://shopping-phinf.pstatic.net/main_2090924/20909247470.20220816165032.jpg"},
-      {supplementName: "감마리놀렌산", imageURL: "https://shopping-phinf.pstatic.net/main_6595389/6595389674.20200818161546.jpg"},
-      {supplementName: "코엔자임Q10", imageURL: "https://shopping-phinf.pstatic.net/main_3532696/35326969621.20221019173459.jpg"}
-    ],
-    contents: [
-      "너무 짠 음식,\n기름진 음식은\n좋지 않아요",
-      "혈액의\n원활한 흐름을 위해 충분히 수분을\n섭취해 주세요",
-      "스트레스는\n만병의 근원, 나만의 스트레스 해소법 찾기!",
-      "견과류\n하루 한 줌으로\n혈관 건강\n함께 챙겨요!"
-    ]
   },
   {
     id: 9,
     title: "피부건강",
     src: `images/healthicons/moisture.png`,
-    supplementsList: [
-      {supplementName: "콜라겐", imageURL: "https://shopping-phinf.pstatic.net/main_2234835/22348357257.20200402154654.jpg"},
-      {supplementName: "스피루리나", imageURL: "https://shopping-phinf.pstatic.net/main_2287826/22878264426.20200516152559.jpg"},
-      {supplementName: "히알루론산", imageURL: "https://shopping-phinf.pstatic.net/main_2215686/22156867863.20220608095424.jpg"},
-      {supplementName: "종합비타민", imageURL: "https://shopping-phinf.pstatic.net/main_1735391/17353911012.20201120160601.jpg"}
-    ],
-    contents: [
-      "매일매일 스킨케어 미루지 않고\n진행해 주세요!",
-      "물을 많이 마시면 피부가 건강해져요",
-      "기름진 음식은\n피부 건강에\n좋지 않아요",
-      "외출할 때 꼭꼭!\n잊지 말고\n선크림 바르고 나가세요!"
-    ]
   },
   {
     id: 10,
     title: "위건강",
     src: `images/healthicons/stomach.png`,
-    supplementsList: [
-      {supplementName: "매스틱 검", imageURL: "https://shopping-phinf.pstatic.net/main_2962220/29622203623.20211110163045.jpg"},
-      {supplementName: "프로폴리스", imageURL: "https://shopping-phinf.pstatic.net/main_3129732/31297322631.20220314114144.jpg"},
-      {supplementName: "소화효소", imageURL: "https://shopping-phinf.pstatic.net/main_8243000/82430008468.1.jpg"},
-      {supplementName: "스페인감초", imageURL: "https://shopping-phinf.pstatic.net/main_8336466/83364667643.10.jpg"}
-    ],
-    contents: [
-      "짜고 맵고 기름진 음식은\n위를 괴롭게 합니다",
-      "끼니 거르지 말고 일정한 시간에\n챙겨 드세요!",
-      "신선한 채소를\n많이 섭취해 주세요!",
-      "지나친 흡연은\n위암을 유발할 수 있어요"
-    ]
   },
   {
     id: 11,
     title: "관절/뼈건강",
     src: `images/healthicons/bone.png`,
-    supplementsList: [
-      {supplementName: "칼슘", imageURL: "https://shopping-phinf.pstatic.net/main_1345221/13452217992.20201214154239.jpg"},
-      {supplementName: "마그네슘", imageURL: "https://shopping-phinf.pstatic.net/main_1309871/13098713037.20210805172525.jpg"},
-      {supplementName: "글루코사민", imageURL: "https://shopping-phinf.pstatic.net/main_2384884/23848842522.20230207142957.jpg"},
-      {supplementName: "MSM", imageURL: "https://shopping-phinf.pstatic.net/main_2850959/28509596557.20210820170430.jpg"}
-    ],
-    contents: [
-      "근력 운동은\n뼈 건강에도\n좋은 영향을 미쳐요!",
-      "뼈 건강을 위해 멸치와 유제품\n꼭 챙겨 드세요!",
-      "체중이 많이 나간다면 좀 더 가볍게\n조절해 주세요",
-      "음식 먹을 때 포화지방 섭취는 줄여주세요"
-    ]
   },
   {
     id: 12,
     title: "갱년기",
     src: `images/healthicons/menopause.png`,
-    supplementsList: [
-      {supplementName: "감마리놀렌산", imageURL: "https://shopping-phinf.pstatic.net/main_6595389/6595389674.20200818161546.jpg"},
-      {supplementName: "피크노제놀", imageURL: "https://shopping-phinf.pstatic.net/main_3536709/35367098618.20221021150849.jpg"},
-      {supplementName: "회화나무\n열매추출물", imageURL: "https://shopping-phinf.pstatic.net/main_8434906/84349060781.jpg"},
-      {supplementName: "마그네슘", imageURL: "https://shopping-phinf.pstatic.net/main_1309871/13098713037.20210805172525.jpg"}
-    ],
-    contents: [
-      "주변 사람과\n자주 대화하며\n우울한 감정을 풀어보세요",
-      "균형잡힌 식사와 꾸준한 운동으로 건강을 관리해 주세요",
-      "집 주변을 산책하며 햇볕을 쬐는 건 어떠신가요?",
-      "뼈 건강을 위해 멸치와 유제품\n꼭 챙겨 드세요!"
-    ]
   },
   {
     id: 13,
     title: "기억력개선",
     src: `images/healthicons/brain.png`,
-    supplementsList: [
-      {supplementName: "홍삼", imageURL: "https://shopping-phinf.pstatic.net/main_1125965/11259655619.20210323174551.jpg"},
-      {supplementName: "오메가3", imageURL: "https://shopping-phinf.pstatic.net/main_2491614/24916146522.20201120102440.jpg"},
-      {supplementName: "은행잎추출물", imageURL: "https://shopping-phinf.pstatic.net/main_8182440/81824407982.1.jpg"},
-      {supplementName: "포스파티딜세린", imageURL: "https://shopping-phinf.pstatic.net/main_3476793/34767932619.20230302102017.jpg"}
-    ],
-    contents: [
-      "기억력 개선에는 호두와 연어 섭취가 도움이 돼요!",
-      "충분한 수면으로\n뇌가 정보를 정리할 시간을 주세요",
-      "새로운 분야에 도전하여\n뇌에 자극을 주는 건 어떠신가요?",
-      "사람들을 많이 만나고 대화를 많이 나누는 것도 좋습니다"
-    ]
   },
   {
     id: 14,
     title: "긴장완화",
     src: `images/healthicons/relax.png`,
-    supplementsList: [
-      {supplementName: "마그네슘", imageURL: "https://shopping-phinf.pstatic.net/main_1309871/13098713037.20210805172525.jpg"},
-      {supplementName: "비타민B", imageURL: "https://shopping-phinf.pstatic.net/main_1735391/17353911012.20201120160601.jpg"},
-      {supplementName: "L-테아닌", imageURL: "https://shopping-phinf.pstatic.net/main_3619100/36191005618.20221130093830.jpg"},
-      {supplementName: "홍경천", imageURL: "https://shopping-phinf.pstatic.net/main_3604167/36041677619.20221123104955.jpg"}
-    ],
-    contents: [
-      "명상이나 요가로 정신을 안정시킬 수 있습니다.",
-      "편안한 자세로 호흡에만 집중하며 긴장을 완화시켜 보세요!",
-      "스트레스는\n만병의 근원, 나만의 스트레스 해소법 찾기!",
-      "마음 속으로\n‘다 잘 될 거야’를 외쳐 보세요!"
-    ]
   },
   {
     id: 15,
     title: "성장발육",
     src: `images/healthicons/grow.png`,
-    supplementsList: [
-      {supplementName: "키즈 비타민", imageURL: "https://shopping-phinf.pstatic.net/main_8306891/83068918634.4.jpg"},
-      {supplementName: "키즈 칼슘", imageURL: "https://shopping-phinf.pstatic.net/main_1545815/15458155124.20220401124817.jpg"}
-    ],
-    contents: [
-      "편식하지 않고 영양분을 골고루 섭취할 수 있게 해주세요!",
-      "활발한 야외 활동으로 아이들은 쑥쑥 자라나요",
-      "인스턴트, 가공식품을 너무 많이\n먹지 않도록\n주의해 주세요",
-      "규칙적인 생활습관을 유지할 수 있도록 지도해 주세요!"
-    ]
   },
   {
     id: 16,
     title: "혈당조절",
     src: `images/healthicons/bloodsugar.png`,
-    supplementsList: [
-      {supplementName: "바나바잎추출물", imageURL: "https://shopping-phinf.pstatic.net/main_3057232/30572321618.20220119110050.jpg"},
-      {supplementName: "난소화성\n말토덱스트린", imageURL: "https://shopping-phinf.pstatic.net/main_3158567/31585674065.jpg"},
-      {supplementName: "크로뮴", imageURL: "https://shopping-phinf.pstatic.net/main_8281040/82810403311.jpg"},
-      {supplementName: "이눌린", imageURL: "https://shopping-phinf.pstatic.net/main_1198803/11988034721.5.jpg"}
-    ],
-    contents: [
-      "간헐적 단식으로 체중도 줄이고\n혈당도 조절하세요!",
-      "정제된 곡물 대신 통곡물을 많이\n섭취해 주세요",
-      "혈당 조절에는\n꾸준한 운동도\n많은 도움이 돼요!",
-      "충분한 수면으로 인슐린 능력을 높여주세요"
-    ]
   },
   {
     id: 17,
     title: "혈압조절",
     src: `images/healthicons/heart-rate.png`,
-    supplementsList: [
-      {supplementName: "코엔자임Q10", imageURL: "https://shopping-phinf.pstatic.net/main_3532696/35326969621.20221019173459.jpg"},
-      {supplementName: "칼륨", imageURL: "https://shopping-phinf.pstatic.net/main_3000225/30002252618.20211208143706.jpg"},
-      {supplementName: "폴리코사놀", imageURL: "https://shopping-phinf.pstatic.net/main_2360474/23604744490.20200728153746.jpg"},
-      {supplementName: "GABA", imageURL: "https://shopping-phinf.pstatic.net/main_3397818/33978181619.20220818084027.jpg"}
-    ],
-    contents: [
-      "짠 음식 섭취는 혈압을 높게 만들어요",
-      "규칙적으로 꾸준히 운동을 진행해 주세요!",
-      "체중이 많이 나간다면 좀 더 가볍게\n조절해 주세요",
-      "술과 담배는 가급적 멀리해 주세요"
-    ]
   },
   {
     id: 18,
     title: "항산화",
     src: `images/healthicons/cell.png`,
-    supplementsList: [
-      {supplementName: "코엔자임Q10", imageURL: "https://shopping-phinf.pstatic.net/main_3532696/35326969621.20221019173459.jpg"},
-      {supplementName: "프로폴리스", imageURL: "https://shopping-phinf.pstatic.net/main_3129732/31297322631.20220314114144.jpg"},
-      {supplementName: "비타민C", imageURL: "https://shopping-phinf.pstatic.net/main_2685541/26855413531.20220719084339.jpg"},
-      {supplementName: "셀레늄", imageURL: "https://shopping-phinf.pstatic.net/main_1623678/16236788013.20210719141606.jpg"}
-    ],
-    contents: [
-      "블루베리는\n눈에도 좋지만 뛰어난 항산화 음식이기도 합니다.",
-      "간식을 끊을 수 없다면, 다크초콜릿 한 조각 어떠신가요?",
-      "튀기고 구운 음식보다 찌거나 삶은 음식이 좋아요!",
-      "정제된 곡물 대신 통곡물을 많이\n섭취해 주세요"
-    ]
   },
   {
     id: 19,
     title: "전립선",
     src: `images/healthicons/prostate.png`,
-    supplementsList: [
-      {supplementName: "쏘팔메토", imageURL: "https://shopping-phinf.pstatic.net/main_1032692/10326922248.20220921181158.jpg"},
-      {supplementName: "아연", imageURL: "https://shopping-phinf.pstatic.net/main_1860252/18602526124.20200814113559.jpg"},
-      {supplementName: "옥타코사놀", imageURL: "https://shopping-phinf.pstatic.net/main_2237563/22375636422.20221128122006.jpg"}
-    ],
-    contents: [
-      "오래 앉아 있었다면 바로 일어나 스트레칭해주세요!",
-      "규칙적인\n생활습관 형성이 중요해요",
-      "토마토, 양파, 마늘을 많이 섭취해 주세요!",
-      "규칙적으로 꾸준히 운동을 진행해 주세요!"
-    ]
   },
   {
     id: 20,
     title: "콜레스테롤",
     src: `images/healthicons/cholesterol.png`,
-    supplementsList: [
-      {supplementName: "감마리놀렌산", imageURL: "https://shopping-phinf.pstatic.net/main_6595389/6595389674.20200818161546.jpg"},
-      {supplementName: "레시틴", imageURL: "https://shopping-phinf.pstatic.net/main_1471290/14712906888.20230125113205.jpg"},
-      {supplementName: "차전자피", imageURL: "https://shopping-phinf.pstatic.net/main_2675726/26757266524.20221129172915.jpg"},
-      {supplementName: "키토산", imageURL: "https://shopping-phinf.pstatic.net/main_1937838/19378383673.20220504115822.jpg"}
-    ],
-    contents: [
-      "포화지방과 콜레스테롤이 많은 음식을 피해주세요",
-      "끼니 거르지 말고 일정한 시간에\n챙겨 드세요!",
-      "등 푸른 생선 섭취로 나쁜 콜레스테롤을 줄일 수 있어요!",
-      "체중이 많이 나간다면 좀 더 가볍게\n조절해 주세요"
-    ]
   },
 ]
